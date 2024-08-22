a7598ae22b37f93de91fc02582b42d51019c5566
abdelrahman027 <116889922+abdelrahman027@users.noreply.github.com>
2024-08-22 13:07:17 +0300

    Edit admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Project,ProjectImage
+from .models import Project,ProjectImage,Category
 # Register your models here.
 
 admin.site.register(Project)
 admin.site.register(ProjectImage)
+admin.site.register(Category)
+
